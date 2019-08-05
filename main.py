5c09dfc77f2c3978aa4ab9645776f49d52c10322
lulzx <wiserishabh@gmail.com>
2019-08-05 10:49:49 +0530

    added files

--- main.py
+++ main.py
@@ -37,109 +37,6 @@ def before_processing(update, context):
         update.effective_message.reply_text(text=text)
         context.bot.leave_chat(chat_id=update.effective_message.chat_id)
         raise DispatcherHandlerStop
-    else:
-        pass
-
-
-@run_async
-def process_message(update, context, remove_caption=False, custom_caption=None):
-    reply_markup = InlineKeyboardMarkup([[
-        InlineKeyboardButton("🌐 Add url buttons", callback_data='add_url_button'),
-        InlineKeyboardButton("📬 Share", switch_inline_query='message_id'),
-        InlineKeyboardButton("📢 Publish to channel", callback_data='channel_id')]])
-    if update.edited_message:
-        message = update.edited_message
-    elif remove_caption:
-        message = update.message.reply_to_message
-    elif custom_caption is not None:
-        message = update.message.reply_to_message
-    else:
-        message = update.message
-
-    if custom_caption is None:
-        caption = message.caption_html if (message.caption and remove_caption is False) else None
-    else:
-        caption = custom_caption
-
-    if message.text:
-        message.reply_text(text=message.text_html, parse_mode=ParseMode.HTML)
-
-    elif message.voice:
-        media = message.voice.file_id
-        duration = message.voice.duration
-        message.reply_voice(voice=media, duration=duration, caption=caption, parse_mode=ParseMode.HTML)
-
-    elif message.photo:
-        media = message.photo[-1].file_id
-        message.reply_photo(photo=media, caption=caption, parse_mode=ParseMode.HTML)
-
-    elif message.sticker:
-        media = message.sticker.file_id
-        message.reply_sticker(sticker=media)
-
-    elif message.document:
-        media = message.document.file_id
-        filename = message.document.file_name
-        message.reply_document(document=media, filename=filename, caption=caption, parse_mode=ParseMode.HTML)
-
-    elif message.audio:
-        media = message.audio.file_id
-        duration = message.audio.duration
-        performer = message.audio.performer
-        title = message.audio.title
-        message.reply_audio(
-            audio=media,
-            duration=duration,
-            performer=performer,
-            title=title,
-            caption=caption,
-            parse_mode=ParseMode.HTML)
-
-    elif message.video:
-        media = message.video.file_id
-        duration = message.video.duration
-        message.reply_video(video=media, duration=duration, caption=caption, parse_mode=ParseMode.HTML)
-
-    elif message.contact:
-        phone_number = message.contact.phone_number
-        first_name = message.contact.first_name
-        last_name = message.contact.last_name
-        message.reply_contact(
-            phone_number=phone_number,
-            first_name=first_name,
-            last_name=last_name)
-
-    elif message.venue:
-        longitude = message.venue.location.longitude
-        latitude = message.venue.location.latitude
-        title = message.venue.title
-        address = message.venue.address
-        foursquare_id = message.venue.foursquare_id
-        message.reply_venue(
-            longitude=longitude,
-            latitude=latitude,
-            title=title,
-            address=address,
-            foursquare_id=foursquare_id)
-
-    elif message.location:
-        longitude = message.location.longitude
-        latitude = message.location.latitude
-        message.reply_location(latitude=latitude, longitude=longitude)
-
-    elif message.video_note:
-        media = message.video_note.file_id
-        length = message.video_note.length
-        duration = message.video_note.duration
-        message.reply_video_note(video_note=media, length=length, duration=duration)
-
-    elif message.game:
-        text = "Sorry, telegram doesn't allow to echo this message"
-        message.reply_text(text=text, quote=True)
-
-    else:
-        text = "Sorry, this kind of media is not supported yet"
-        message.reply_text(text=text, quote=True)
 
 
 class MWT(object):
@@ -166,11 +63,9 @@ class MWT(object):
             key = (args, tuple(kw))
             try:
                 v = self.cache[key]
-                print("cache")
                 if (time.time() - v[1]) > self.timeout:
                     raise KeyError
             except KeyError:
-                print("new")
                 v = self.cache[key] = f(*args, **kwargs), time.time()
             return v[0]
 
@@ -314,9 +209,109 @@ def inline_handler(update, context):
                                  reply_markup=ReplyKeyboardRemove())
 
 
-@restricted
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+@run_async
+def process_message(update, context, remove_caption=False, custom_caption=None):
+    reply_markup = InlineKeyboardMarkup([[
+        InlineKeyboardButton("🌐 Add url buttons", callback_data='add_url_button')],[
+        InlineKeyboardButton("📢 Publish to channel", callback_data='channel_id;announce')],[
+        InlineKeyboardButton("🔕 Publish to channel (silent)", callback_data='channel_id;silent')],[
+        InlineKeyboardButton("📬 Share", switch_inline_query='message_id')]])
+    if update.edited_message:
+        message = update.edited_message
+    elif remove_caption:
+        message = update.message.reply_to_message
+    elif custom_caption is not None:
+        message = update.message.reply_to_message
+    else:
+        message = update.message
+
+    if custom_caption is None:
+        caption = message.caption_html if (message.caption and remove_caption is False) else None
+    else:
+        caption = custom_caption
+
+    if message.text:
+        message.reply_text(text=message.text_html, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+    elif message.voice:
+        media = message.voice.file_id
+        duration = message.voice.duration
+        message.reply_voice(voice=media, duration=duration, caption=caption, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+    elif message.photo:
+        media = message.photo[-1].file_id
+        message.reply_photo(photo=media, caption=caption, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+    elif message.sticker:
+        media = message.sticker.file_id
+        message.reply_sticker(sticker=media, reply_markup=reply_markup)
+
+    elif message.document:
+        media = message.document.file_id
+        filename = message.document.file_name
+        message.reply_document(document=media, filename=filename, caption=caption, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+    elif message.audio:
+        media = message.audio.file_id
+        duration = message.audio.duration
+        performer = message.audio.performer
+        title = message.audio.title
+        message.reply_audio(
+            audio=media,
+            duration=duration,
+            performer=performer,
+            title=title,
+            caption=caption,
+            reply_markup=reply_markup,
+            parse_mode=ParseMode.HTML)
+
+    elif message.video:
+        media = message.video.file_id
+        duration = message.video.duration
+        message.reply_video(video=media, duration=duration, caption=caption, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+    elif message.contact:
+        phone_number = message.contact.phone_number
+        first_name = message.contact.first_name
+        last_name = message.contact.last_name
+        message.reply_contact(
+            phone_number=phone_number,
+            first_name=first_name,
+            last_name=last_name,
+            reply_markup=reply_markup)
+
+    elif message.venue:
+        longitude = message.venue.location.longitude
+        latitude = message.venue.location.latitude
+        title = message.venue.title
+        address = message.venue.address
+        foursquare_id = message.venue.foursquare_id
+        message.reply_venue(
+            longitude=longitude,
+            latitude=latitude,
+            title=title,
+            address=address,
+            foursquare_id=foursquare_id,
+            reply_markup=reply_markup)
+
+    elif message.location:
+        longitude = message.location.longitude
+        latitude = message.location.latitude
+        message.reply_location(latitude=latitude, longitude=longitude, reply_markup=reply_markup)
+
+    elif message.video_note:
+        media = message.video_note.file_id
+        length = message.video_note.length
+        duration = message.video_note.duration
+        message.reply_video_note(video_note=media, length=length, duration=duration, reply_markup=reply_markup)
+
+    elif message.game:
+        text = "Sorry, telegram doesn't allow to echo this message"
+        message.reply_text(text=text, quote=True)
+
+    else:
+        text = "Sorry, this kind of media is not supported yet"
+        message.reply_text(text=text, quote=True)
 
 
 def inline(update, context, switch_pm=None):
@@ -357,10 +352,29 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
+
+            PHOTO: [MessageHandler(Filters.photo, photo),
+                    CommandHandler('skip', skip_photo)],
+
+            LOCATION: [MessageHandler(Filters.location, location),
+                       CommandHandler('skip', skip_location)],
+
+            BIO: [MessageHandler(Filters.text, bio)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
     dp.add_handler(CommandHandler("calendar", calendar_handler))
     dp.add_handler(CallbackQueryHandler(inline_handler))
     dp.add_handler(InlineQueryHandler(inline))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(~Filters.command, process_message))
     dp.add_error_handler(MessageHandler(Filters.command, unknown))
     dp.add_error_handler(error)
     updater.start_polling()
